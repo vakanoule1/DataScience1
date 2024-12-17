487f863147384ab7bdf1118da34c4b9cf00c976d
vakanoule1 <vdiaby1@gmail.com>
2024-12-16 23:33:55 -0500

    Update cleaner.py

--- hr_monitoring_data_processing/cleaner.py
+++ hr_monitoring_data_processing/cleaner.py
@@ -8,7 +8,23 @@ def filter_nondigits(data: list) -> list:
     Returns:
         list[int]: list of integers, with all non-digit strings removed
     """
-    pass
+     # Create a new empty list.
+    if len(data) == 0 or "":
+        return data
+    
+    new_data = []
+    # using a loop
+    for rate in data:
+
+    # check if my rate is a digit (consideing \n)
+        rate = rate.strip()
+        if rate.isdigit():
+
+    # if my rate is a digit append it to new_data
+            new_data.append(int(rate))
+        else:
+            rate = rate
+    return new_data
 
 
 def filter_outliers(data: list) -> list:
