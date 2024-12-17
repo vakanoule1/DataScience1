d1f0d2287ab8a7dd4984d2eb6e7c8f629b355934
vakanoule1 <vdiaby1@gmail.com>
2024-12-16 23:46:46 -0500

    Update metrics.py

--- hr_monitoring_data_processing/metrics.py
+++ hr_monitoring_data_processing/metrics.py
@@ -23,8 +23,66 @@ def window_max(data: list, n: int) -> list:
 
 
 def window_average(data: list, n: int) -> list:
-    pass
+    """
+    Calculate the moving average of 'n'- size parameter
+
+    Args:
+        data (list[int]): list of integers representing heart rate samples
+        n (int): The size of your window
+    Returns:
+        list[int]: the moving average with window size 6
+    """
+    # Program to calclate moving average
+    # Initialize an empty array to hold the resulting data points.
+    average_list = []
+
+    # loop through the original array n - window_size + 1 times.
+    # where n is the initial number of elements.
+    for i in range(0,len(data), n):
+        window = data[i:i + n]
+
+    # Calculate the average of a sequence
+        average = sum(window) / len(window)
+
+    #Append the newly calculated window average to our result array
+        average_list.append(round(average, 2))
+        
+    return average_list
 
 
 def window_stddev(data: list, n: int) -> list:
-    pass
+    """
+    Calculate the standard deviation of 'n'- size parameter
+
+    Args:
+        data (list[int]): list of integers representing heart rate samples
+        n (int): The size of your window
+    Returns:
+        list[int]: A list containing the standard deviations with window size 6
+    """
+    import statistics
+    if data == []:
+        return data
+    
+    # Create am empty list to store standard  deviations
+    stddev = []
+
+    # Using a loop to iterate through the data
+    # starting from the first element and ending at the index
+    for i in range(0,len(data), n):
+    
+    # Create a slice of data representing the current window of size n.
+        window_stddev = data[i : i + n]
+
+        if len(window_stddev) == 1:
+            continue
+        std_dev = statistics.stdev(window_stddev)
+
+    # The window_stddev() function computes the standard deviation of the current window
+    # Append it to std_values
+        stddev.append(round(std_dev, 2))
+        
+
+    # Return the list containing the calculated standard deviattions for neach window 
+    return stddev
+
