bee21afabdef57efaaf67a79893289274105e8c7
vakanoule1 <vdiaby1@gmail.com>
2024-12-16 23:36:11 -0500

    Update cleaner.py

--- hr_monitoring_data_processing/cleaner.py
+++ hr_monitoring_data_processing/cleaner.py
@@ -28,4 +28,34 @@ def filter_nondigits(data: list) -> list:
 
 
 def filter_outliers(data: list) -> list:
-    pass
+    """
+    Filter all integers from list that are less or equal 30 and greater than or equal to 250
+
+    Arg:
+        data (list[str]):list of strings representing heart rate samples.
+            Might contain invalid or missing data.
+    Returns:
+        list[int]: list of integers less than 30 and greater than 250, with all non-digit strings removed
+    """
+    
+    
+# Create a new empty list.
+    
+    if len(data) == 0:
+        return data
+    
+    new_data = []
+    # using a loop
+    # Take in integers
+    for rate in data:
+
+     # check if my rate is less or equal to 30 and greater or equal to 250.
+        if rate > 30 and rate < 250:
+           
+    # if my rate is less than 30 and greater than 250 append it to new_data
+           new_data.append(rate)
+        else:
+            rate = rate
+    return new_data
+        
+
