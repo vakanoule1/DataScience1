2fb3cde4e792df5bda54f6597501207d0051d249
vakanoule1 <vdiaby1@gmail.com>
2024-12-16 23:43:25 -0500

    Update metrics.py

--- hr_monitoring_data_processing/metrics.py
+++ hr_monitoring_data_processing/metrics.py
@@ -8,8 +8,18 @@ def window_max(data: list, n: int) -> list:
     Returns:
         list[int]: list of maximums from each window (size should be len(data)//6)
     """
+    # initialises the output data
     maximums = []
-    ...
+    
+    #using a loop to take in intgers in range
+    for i in range(0,len(data), n):
+
+    # temp variable is used to store the maximum value of the sliding window
+        temp = data[i: i + n]
+
+    # temp variable is appended to maximums
+        maximums.append(max(temp))
+    return maximums
 
 
 def window_average(data: list, n: int) -> list:
